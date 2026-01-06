4c44547723d0edd8b29646e185790ef05bf3a9bf
awanzada786616 <wasicenter120@gmail.com>
2026-01-06 17:22:28 +0500

    Update balance.js

--- api/balance.js
+++ api/balance.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
-  const url = `http://otpget.com/stubs/handler_api.php?api_key=${process.env.OTP_API_KEY}&action=getBalance`;
+  const API_KEY = "bayysjurkagxbup3nqqwq8j5f5vn8jid";
 
+  const url = `http://otpget.com/stubs/handler_api.php?api_key=${API_KEY}&action=getBalance`;
   const r = await fetch(url);
   const text = await r.text();
 
