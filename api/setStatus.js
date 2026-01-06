97d81a47bc7fe8c99edd74b3b97057bdbd2b4a94
awanzada786616 <wasicenter120@gmail.com>
2026-01-06 18:09:48 +0500

    Update setStatus.js

--- api/setStatus.js
+++ api/setStatus.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
+  const API_KEY = "YOUR_API_KEY_HERE";
   const { id, status } = req.query;
 
-  const url = `http://otpget.com/stubs/handler_api.php?api_key=${process.env.OTP_API_KEY}&action=setStatus&id=${id}&status=${status}`;
-
+  const url = `http://otpget.com/stubs/handler_api.php?api_key=${API_KEY}&action=setStatus&id=${id}&status=${status}`;
   const r = await fetch(url);
   const text = await r.text();
 
