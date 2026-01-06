9332d84eef624c9f26bb29ae7591150e965ec901
awanzada786616 <wasicenter120@gmail.com>
2026-01-06 18:16:17 +0500

    Update getNumber.js

--- api/getNumber.js
+++ api/getNumber.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const API_KEY = "bayysjurkagxbup3nqqwq8j5f5vn8jid";
-  const { service, country, type } = req.query;
+  const { country, service, type } = req.query;
 
   const url = `http://otpget.com/stubs/handler_api.php?api_key=${API_KEY}&action=getNumber&service=${service}&country=${country}&type=${type}`;
   const r = await fetch(url);
