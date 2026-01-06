ad00cb45f1b55eb9ac0938ffce0866d96302e10c
awanzada786616 <wasicenter120@gmail.com>
2026-01-06 17:13:59 +0500

    Create services.js

---
+++ api/services.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  const { country, type } = req.query;
+
+  const url = `http://otpget.com/stubs/handler_api.php?api_key=${process.env.OTP_API_KEY}&action=getServices&country=${country}&type=${type}`;
+
+  const r = await fetch(url);
+  const text = await r.text();
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).send(text);
+}
